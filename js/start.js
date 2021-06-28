a8b8347834a2e16cf6b84b76f47451c03d2b3061
moonmonoon <moonmonoon@gmail.com>
2021-06-29 00:36:00 +0900

    Add qna page and buttons

--- js/start.js
+++ js/start.js
@@ -1,6 +1,29 @@
 const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
 
+function addAnswer(answerText, qIdx){
+  var a = document.querySelector('.answerBox');
+  var answer = document.createElement('button');
+  answer.classList.add('answerList');
+  a.appendChild(answer); //answer가 a에 소속되도록 관계를 만듬
+  answer.innerHTML = answerText;
+
+  answer.addEventListener("click", function(){ //버튼 클릭하면 화면의 모든 버튼 사라짐
+    var children = document.querySelectorAll('.answerList');
+    for(let i = 0; i < children.length; i++){
+      children[i].disabled = true;
+      children[i].style.display = 'none';
+    }
+    goNext(++qIdx);
+    }, false);
+}
+function goNext(qIdx){
+  var q = document.querySelector('.qBox');
+  q.innerHTML = qnaList[qIdx].q;
+  for(let i in qnaList[qIdx].a){
+    addAnswer(qnaList[qIdx].a[i].answer, qIdx);
+  }
+}
 function begin(){
   main.style.WebkitAnimation = "fadeOut 1s";
   main.style.animation = "fadeOut 1s";
@@ -11,5 +34,7 @@ function begin(){
       main.style.display = "none";
       qna.style.display = "block";
     }, 450)
+    let qIdx = 0;
+    goNext(qIdx);
   }, 450);
 }
