d18c43bf27b5b08dbe629520c09d25001cf0b0b0
moonmonoon <moonmonoon@gmail.com>
2021-06-30 15:28:38 +0900

    Add function calculate result

--- js/start.js
+++ js/start.js
@@ -2,21 +2,65 @@ const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
 const result = document.querySelector("#result");
 const endPoint = 12;
+const select = [];
+
+function calResult() { // 가장 많이 선택한 타입(동물) 계산
+  var pointArray = [
+    { name: 'mouse', value: 0, key: 0 },
+    { name: 'cow', value: 0, key: 1 },
+    { name: 'tiger', value: 0, key: 2 },
+    { name: 'rabbit', value: 0, key: 3 },
+    { name: 'dragon', value: 0, key: 4 },
+    { name: 'snake', value: 0, key: 5 },
+    { name: 'horse', value: 0, key: 6 },
+    { name: 'sheep', value: 0, key: 7 },
+    { name: 'monkey', value: 0, key: 8 },
+    { name: 'chick', value: 0, key: 9 },
+    { name: 'dog', value: 0, key: 10 },
+    { name: 'pig', value: 0, key: 11 },
+  ]
+
+  for (let i = 0; i < endPoint; i++) {
+    var target = qnaList[i].a[select[i]];
+    for (let j = 0; j < target.type.length; j++) {
+      for (let k = 0; k < pointArray.length; k++) {
+        if (target.type[j] === pointArray[k].name) {
+          pointArray[k].value += 1;
+        }
+      }
+    }
+  }
+
+  var resultArray = pointArray.sort(function(a, b) {
+    if (a.value > b.value) {
+      return -1;
+    }
+    if (a.value < b.value) {
+      return 1;
+    }
+    return 0;
+  });
+  console.log(resultArray); //제대로 함수를 부여했는지
+  let resultword = resultArray[0].key;
+  return resultword;
+}
 
 function goResult() {
   qna.style.WebkitAnimation = "fadeOut 1s";
   qna.style.animation = "fadeOut 1s";
   setTimeout(() => {
-      result.style.WebkitAnimation = "fadeIn 1s";
-      result.style.animation = "fadeIn 1s";
-      setTimeout(() => {
-        qna.style.display = "none";
-        result.style.display = "block";
-      }, 450)
-  })  // syntax error에 주의하자..
+    result.style.WebkitAnimation = "fadeIn 1s";
+    result.style.animation = "fadeIn 1s";
+    setTimeout(() => {
+      qna.style.display = "none";
+      result.style.display = "block";
+    }, 450)
+  }) // syntax error에 주의하자..
+  console.log(select); //선택한 번호 배열 출력
+  calResult();
 }
 
-function addAnswer(answerText, qIdx) {
+function addAnswer(answerText, qIdx, idx) {
   var a = document.querySelector('.answerBox');
   var answer = document.createElement('button');
   answer.classList.add('answerList');
@@ -37,6 +81,7 @@ function addAnswer(answerText, qIdx) {
       // children[i].style.display = 'none';
     }
     setTimeout(() => {
+      select[qIdx] = idx; // 몇번째 질문에서 몇번 버튼을 선택했는지
       for (let i = 0; i < children.length; i++) {
         children[i].style.display = 'none';
       }
@@ -46,7 +91,7 @@ function addAnswer(answerText, qIdx) {
 }
 
 function goNext(qIdx) {
-  if(qIdx === endPoint){
+  if (qIdx === endPoint) {
     goResult();
     return;
   }
@@ -54,7 +99,7 @@ function goNext(qIdx) {
   var q = document.querySelector('.qBox');
   q.innerHTML = qnaList[qIdx].q;
   for (let i in qnaList[qIdx].a) {
-    addAnswer(qnaList[qIdx].a[i].answer, qIdx);
+    addAnswer(qnaList[qIdx].a[i].answer, qIdx, i);
   }
   var status = document.querySelector('.statusBar');
   status.style.width = (100 / endPoint) * (qIdx + 1) + '%';
