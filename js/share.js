b6e9c3ac4c96e0f950c8cc3b71081ccfe5a2b561
moonmonoon <moonmonoon@gmail.com>
2021-07-03 00:30:07 +0900

    Fix typo

--- js/share.js
+++ js/share.js
@@ -2,21 +2,21 @@ const url = 'https://animalmbti-lovetype2.netlify.app/';
 
 function setShare() {
   var resultImg = document.querySelector('#resultImg');
-  var resultAlt = resultImg.firstElementChile.alt;
+  var resultAlt = resultImg.firstElementChild.alt;
   const shareTitle = '동물로 알아보는 연애유형 결과';
   const shareDes = infoList[resultAlt].name;
   const shareImage = url + 'img/image-' + resultAlt + '.png';
-  const shareURL = url + 'page.result-' + resultAlt + '.html';
+  const shareURL = url + 'page/result-' + resultAlt + '.html';
 
   Kakao.Link.sendDefault({
       objectType: 'feed',
       content: {
-        title: 'shareTitle',
-        description: 'shareDes',
-        imageUrl: 'shareImage',
+        title: shareTitle,
+        description: shareDes,
+        imageUrl: shareImage,
         link: {
           mobileWebUrl: shareURL,
-          WebUrl: shareURL,
+          webUrl: shareURL
         },
       },
 
@@ -24,10 +24,8 @@ function setShare() {
           title: '결과 구경하기',
           link: {
             mobileWebUrl: shareURL,
-            WebUrl: shareURL
+            webUrl: shareURL
           },
-        },
-      },
-    ]
+        }]
   });
 }
