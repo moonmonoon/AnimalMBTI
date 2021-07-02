5ce9b4d72769ce1c261a2594d19ee05b7705a998
moonmonoon <moonmonoon@gmail.com>
2021-07-03 00:07:59 +0900

    Fix share function

--- js/share.js
+++ js/share.js
@@ -1,30 +1,31 @@
-function kakaoShare() {
+const url = 'https://animalmbti-lovetype2.netlify.app/';
+
+function setShare() {
+  var resultImg = document.querySelector('#resultImg');
+  var resultAlt = resultImg.firstElementChile.alt;
+  const shareTitle = '동물로 알아보는 연애유형 결과';
+  const shareDes = infoList[resultAlt].name;
+  const shareImage = url + 'img/image-' + resultAlt + '.png';
+  const shareURL = url + 'page.result-' + resultAlt + '.html';
+
   Kakao.Link.sendDefault({
-    objectType: 'feed',
-    content: {
-      title: '디저트 사진',
-      description: '아메리카노, 빵, 케익',
-      imageUrl: 'http://mud-kage.kakao.co.kr/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg',
-      link: {
-        mobileWebUrl: 'https://developers.kakao.com',
-        androidExecutionParams: 'test',
-      },
-    },
-    social: {
-      likeCount: 10,
-      commentCount: 20,
-      sharedCount: 30,
-    },
-    buttons: [{
-        title: '웹으로 이동',
+      objectType: 'feed',
+      content: {
+        title: 'shareTitle',
+        description: 'shareDes',
+        imageUrl: 'shareImage',
         link: {
-          mobileWebUrl: 'https://developers.kakao.com',
+          mobileWebUrl: shareURL,
+          WebUrl: shareURL,
         },
       },
-      {
-        title: '앱으로 이동',
-        link: {
-          mobileWebUrl: 'https://developers.kakao.com',
+
+      buttons: [{
+          title: '결과 구경하기',
+          link: {
+            mobileWebUrl: shareURL,
+            WebUrl: shareURL
+          },
         },
       },
     ]
